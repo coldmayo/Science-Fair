b2c53cb3193ea7eeb72148a1d5b78a079b0e5b92
wallachmaya <52044992+wallachmaya@users.noreply.github.com>
2019-12-22 07:51:09 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,20 +1,22 @@
-import re
-from collections import Counter
-from nltk.corpus import stopwords
-def counter(numb,doc,namee):
-    words = re.findall(r'\w+', open(doc).read().lower())
-    stop_words = set(stopwords.words('english'))
-    words = [w for w in words if not w in stop_words]
-    count = Counter(words).most_common(numb)
-    print(namee,"\n")
-    return(count)
-#El Paso Shooter numb=110
-print(counter(110,'El Paso Shooter.txt', 'El Paso Shooter'))
-#Elliot Rodger numb=200
-print(counter(200,'Eloit Rodger.txt', 'Eloit Rodger'))
-#Jim David Adkisson numb=120
-print(counter(120,'Jim David Adkisson.txt', 'Jim David Adkisson'))
-#Dylann Roof numb=150
-print(counter(150,'Dylann Roof.txt', 'Dylann Roof'))
-#Seuong Cho
-print(counter(140,'Sueong Cho.txt', 'Seuong Cho'))
+import csv
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from textblob.classifiers import NaiveBayesClassifier as NBC
+from textblob import TextBlob
+
+training_corpus = []
+test_corpus = []
+
+with open('train.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        training_corpus.append((row['tweets'], row['Classification']))
+
+with open('train2.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        test_corpus.append((row['tweets'], row['Classification']))
+
+model = NBC(training_corpus) 
+print(model.classify(""))
+print(model.accuracy(test_corpus))
