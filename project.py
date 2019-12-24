ccba59b6c425b33a3db6e38fbd7c6fbf0fb8ac95
wallachmaya <52044992+wallachmaya@users.noreply.github.com>
2019-12-23 20:59:06 -0500

    Update project.py

--- project.py
+++ project.py
@@ -6,6 +6,8 @@ from textblob import TextBlob
 training_corpus = []
 test_corpus = []
 test_tweets = []
+y_pred= []
+y_true= []
 with open('train.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
@@ -20,9 +22,10 @@ with open('test_tweets.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         test_tweets.append((row['tweets']))    
-
+        y_true.append((row['TRUE']))
 model = NBC(training_corpus)
 for tweet in test_tweets:
-    print(model.classify(tweet))
-print(model.classify("What the hell"))
-print(model.accuracy(test_corpus))
+    pred=model.classify(tweet)
+    print(pred)
+    y_pred.append(pred)
+print((model.accuracy(test_corpus))*100,'percent')
