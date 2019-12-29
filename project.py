9b86ef6bba9e2d2ecd6e2261191fd7ace28eb45c
wallachmaya <52044992+wallachmaya@users.noreply.github.com>
2019-12-29 08:09:22 -0500

    Update project.py

--- project.py
+++ project.py
@@ -4,28 +4,27 @@ from textblob.classifiers import NaiveBayesClassifier as NBC
 from textblob import TextBlob
 
 training_corpus = []
-test_corpus = []
 test_tweets = []
 y_pred= []
 y_true= []
+
 with open('train.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         training_corpus.append((row['tweets'], row['Classification']))
-
-with open('train2.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        test_corpus.append((row['tweets'], row['Classification']))
         
 with open('test_tweets.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         test_tweets.append((row['tweets']))    
         y_true.append((row['TRUE']))
+
 model = NBC(training_corpus)
+file = open("Predictions.txt", "w")
+print("Prediction of test_tweets")
 for tweet in test_tweets:
     pred=model.classify(tweet)
+    file.write(pred+'\n') 
     print(pred)
     y_pred.append(pred)
-print((model.accuracy(test_corpus))*100,'percent')
+file.close()
