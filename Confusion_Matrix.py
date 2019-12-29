b79e740d553b81e112ee24a7ee0d200d0157bb41
wallachmaya <52044992+wallachmaya@users.noreply.github.com>
2019-12-29 08:10:16 -0500

    Update Confusion_Matrix.py

--- Confusion_Matrix.py
+++ Confusion_Matrix.py
@@ -4,3 +4,12 @@ from IPython.display import Image
 print(Image(filename='conf.png'))
 print ('\nClasification report:\n', classification_report(y_true,y_pred))
 print ('\nConfussion matrix:\n',confusion_matrix(y_true,y_pred))
+with open("ConfusionMat+ClassRep.txt", 'w') as f:
+    f.write('Confusion Matrix:')
+    f.write('\n')
+    f.write(np.array2string(confusion_matrix(y_true, y_pred), separator=', '))
+    f.write('\n')
+    f.write('Classification Report:')
+    f.write('\n')
+    f.write(classification_report(y_true,y_pred))
+f.close()
