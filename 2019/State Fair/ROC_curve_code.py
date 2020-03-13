6610a00b384a71fb8775a56f28320033ae1db104
wallachmaya <52044992+wallachmaya@users.noreply.github.com>
2020-03-13 14:07:05 -0400

    Update ROC_curve_code.py
    fixed error with roc_curve()

--- 2019/State Fair/ROC_curve_code.py
+++ 2019/State Fair/ROC_curve_code.py
@@ -4,7 +4,7 @@ for i in df2['TRUE']:
         test_labels.append(0)
     else:
         test_labels.append(1)
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import precision_score, recall_score, roc_auc_score, roc_curve
 
 y_pree=[]
 for j in y_pred:
@@ -16,7 +16,7 @@ for j in y_pred:
 # Calculate roc auc
 roc_value = roc_auc_score(test_labels, y_pree)
 base_fpr, base_tpr, _ = roc_curve(test_labels, [1 for _ in range(len(test_labels))])
-model_fpr, model_tpr, _ = roc_curve(test_labels, test_labels)
+model_fpr, model_tpr, _ = roc_curve(test_labels, y_pree)
 
 plt.figure(figsize = (8, 6))
 plt.rcParams['font.size'] = 16
